e03d069f1ce847612a34a69b1e25b312b5808e88
Andrew Pronek <102892202+Qwaggi@users.noreply.github.com>
2022-09-30 11:43:14 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,7 @@
 import './App.scss';
 import { Routes, Route } from 'react-router-dom';
 import Homepage from './components/Homepage'; 
-
-function App() {
-  return (
-    <> 
-    <Routes>
-      <Route path="/" element = {<Homepage/>} />  
-    </Routes>
-    </> 
-  )
-}
-
-export default App;
+import Shop from './components/Shop';
+import Topbar from './components/Topbar';
+import Personal from './components/Personal';
+import Example from"./components/Example";
